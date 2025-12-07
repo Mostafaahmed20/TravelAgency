58f89b823afc9449d5a8ef1bd1c48b52d5518353
mostafa <mostafa.ahmed00q@gmail.com>
2025-12-07 21:26:01 +0200

    Add vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TravelAgency/', // GitHub Pages base URL
-  server: {
-    port: 5173,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild'
-  }
+  base: '/TravelAgency/',
 })
+
