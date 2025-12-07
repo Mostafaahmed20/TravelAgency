18e8f94292a5e399ccb35bd11629cfca28c12ff2
mostafa <mostafa.ahmed00q@gmail.com>
2025-12-07 21:30:33 +0200

    Update vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// For GitHub Pages: If your site is at https://username.github.io/repo-name, use '/repo-name/'
+// If your site is at https://username.github.io (root), use '/'
 export default defineConfig({
   plugins: [react()],
   base: '/TravelAgency/',
